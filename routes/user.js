430174ed9b5e692ce22458640e9241026d64b333
Rejith Reghunathan <rejithreghunathan6378@gmail.com>
2021-02-27 20:22:21 +0530

    collection.js file updated and inserted fisrt user into the db

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../config/connection')
+const collection = require('../config/collection')
 
 router.get('/',(req,res)=>{
-                                                                                                                                                                                            
+                                                                                                                                                                              
 })
 
 module.exports = router;
