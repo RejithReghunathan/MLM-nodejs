9dc4c9d3d34b565cbdd7201aee09b4e5bc4f097c
Rejith Reghunathan <rejithreghunathan6378@gmail.com>
2021-03-01 18:02:39 +0530

    Login doing

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,19 @@
 const db = require('../config/connection')
 const collection = require('../config/collection')
 var objectId = require("mongodb").ObjectID;
+const bcypt = require('bcrypt')
 
 module.exports={
-    
+    userLogin:(data)=>{
+        return new Promise(async(resolve,reject)=>{
+            let status = false
+            let response = {}
+            console.log('data',data);
+            let user = await db.get().collection(collection.USER_COLLECTION).findOne({name:data.email})
+            if(!user){
+                response.status=false
+                resolve(response)
+            }
+        })
+    }
 }
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const db = require('../config/connection')
-const collection = require('../config/collection')
+const userController = require('../controllers/user')
+
 
 router.get('/',(req,res)=>{
   res.render('User/login.hbs')                                    
@@ -9,5 +9,11 @@ router.get('/',(req,res)=>{
 router.get('/register',(req,res)=>{
   res.render('User/register',{name:'UMAR'})
 })
-
+router.post('/login',(req,res)=>{
+  console.log(req.body);
+  userController.userLogin(req.body).then((response)=>{
+    console.log('res',response);
+    res.json(response)
+  })
+})
 module.exports = router;
