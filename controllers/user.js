be76a6fbadcf5287e8d22fe858d6a9f29aec131b
Rejith Reghunathan <rejithreghunathan6378@gmail.com>
2021-03-08 12:14:24 +0530

    User Tree not completetd Still working

--- controllers/user.js
+++ controllers/user.js
@@ -197,59 +197,59 @@ module.exports = {
         })
     },
     tree: () => {
-        class Node {
-            constructor(data) {
-                this.data = data;
-                this.left = null;
-                this.right = null;
-            }
-        }
-        class BinarySearchTree {
-            constructor() {
-                this.root = null;
-            }
-            insert(data) {
+        // class Node {
+        //     constructor(data) {
+        //         this.data = data;
+        //         this.left = null;
+        //         this.right = null;
+        //     }
+        // }
+        // class BinarySearchTree {
+        //     constructor() {
+        //         this.root = null;
+        //     }
+        //     insert(data) {
 
-                var newNode = new Node(data);
-                if (this.root === null){
-                    console.log("Inserted root",data);
-                    this.root = newNode;
-                }
-                else
-                    this.insertNode(this.root, newNode);
-            }
-            insertNode(node, newNode) {
+        //         var newNode = new Node(data);
+        //         if (this.root === null){
+        //             console.log("Inserted root",data);
+        //             this.root = newNode;
+        //         }
+        //         else
+        //             this.insertNode(this.root, newNode);
+        //     }
+        //     insertNode(node, newNode) {
 
-                if (newNode.data < node.data) {
+        //         if (newNode.data < node.data) {
     
-                    if (node.left === null){
-                        console.log("Inserted left");
-                        node.left = newNode;
-                    }
-                    else
+        //             if (node.left === null){
+        //                 console.log("Inserted left");
+        //                 node.left = newNode;
+        //             }
+        //             else
                     
     
-                        this.insertNode(node.left, newNode);
-                } else {
+        //                 this.insertNode(node.left, newNode);
+        //         } else {
     
-                    if (node.right === null){
-                        console.log('Inserted right');
-                        node.right = newNode;
-                    }
-                    else
+        //             if (node.right === null){
+        //                 console.log('Inserted right');
+        //                 node.right = newNode;
+        //             }
+        //             else
     
     
-                        this.insertNode(node.right, newNode);
-                }
-            }
-             postorder(node) {
-                if (node !== null) {
-                    this.postorder(node.left);
-                    this.postorder(node.right);
-                    console.log(node.data);
-                }
-            }
-        }
+        //                 this.insertNode(node.right, newNode);
+        //         }
+        //     }
+        //      postorder(node) {
+        //         if (node !== null) {
+        //             this.postorder(node.left);
+        //             this.postorder(node.right);
+        //             console.log(node.data);
+        //         }
+        //     }
+        // }
 
      
 
@@ -259,24 +259,24 @@ module.exports = {
         var obj = []
         return new Promise(async (resolve, reject) => {
             userFuct(userid)
-            var BST = new BinarySearchTree();
-
             async function userFuct(userid) {
                 let user = await db.get().collection(collection.USER_COLLECTION).findOne({
                     _id: objectId(userid)
                 })
-                BST.insert(user);
-                if (user != null) {
+                if (user!=null) {
+                    async (user)=>{
+                    await obj.push(user)
+                    }
+                    var temp = user
+                    // console.log(temp)
+                    let hello= array.push(user)
+                    console.log(array);
                     userFuct(user.left)
                     userFuct(user.right)
                 }
 
             }
-            console.log("postorder traversal");
-            BST.postorder(userid)
-            resolve()
-
-
+            resolve(obj)
         })
     }
 }
